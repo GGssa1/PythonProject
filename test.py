2a0f247059961735269e80b48fb202ff863f60ad
GGssa1 <59468248+GGssa1@users.noreply.github.com>
2025-05-15 14:25:59 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+#upload pdf file
+st.header("My first Chatbot")
+
+with  st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader(" Upload a PDf file and start asking questions", type="pdf")
+
+#Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+        #st.write(text)
